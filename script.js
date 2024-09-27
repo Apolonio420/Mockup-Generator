377667dcf192d0bd374c30bb2f5d769b71d191fb
Apolonio420 <vwnrvm@gmail.com>
2024-09-27 15:46:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 const estampaPositions = {
     "Remera Oversized": {
       "Front": {
-        "R1": { x: 2400, y: 1000, width: 500, height: 500 },
-        "R2": { x: 1000, y: 1200, width: 900, height: 900 },
-        "R3": { x: 1000, y: 1200, width: 1650, height: 1650 },
+        "R1": { x: 2400, y: 1100, width: 500, height: 500 },
+        "R2": { x: 1500, y: 1100, width: 900, height: 900 },
+        "R3": { x: 1500, y: 1100, width: 1650, height: 1650 },
       },
       "Back": {
-        "R1": { x: 1000, y: 1100, width: 500, height: 500 },
+        "R1": { x: 1000, y: 1300, width: 500, height: 500 },
         "R2": { x: 1000, y: 1750, width: 900, height: 900 },
         "R3": { x: 1000, y: 2000, width: 1650, height: 1650 },
       },
