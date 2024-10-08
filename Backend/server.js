ca2f649f4668281005b4689b7236452c5c5a06bb
Apolonio420 <vwnrvm@gmail.com>
2024-10-08 17:51:57 -0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,8 +21,8 @@ const estampaPositions = {
   "Remera Oversized": {
     "Front": {
       "R1": { x: 2400, y: 1100, width: 500, height: 500 },
-      "R2": { x: 1500, y: 1100, width: 900, height: 900 },
-      "R3": { x: 1200, y: 1000, width: 1650, height: 1650 },
+      "R2": { x: 1800, y: 1100, width: 900, height: 900 },
+      "R3": { x: 1800, y: 1000, width: 1650, height: 1650 },
     },
     "Back": {
       "R1": { x: 1500, y: 1000, width: 500, height: 500 },
@@ -32,14 +32,14 @@ const estampaPositions = {
   },
   "Buzo Oversized": {
     "Front": {
-      "R1": { x: 2400, y: 1100, width: 500, height: 500 },
-      "R2": { x: 1500, y: 1100, width: 900, height: 900 },
-      "R3": { x: 1200, y: 1000, width: 1650, height: 1650 },
+      "R1": { x: 2400, y: 800, width: 500, height: 500 },
+      "R2": { x: 1800, y: 700, width: 900, height: 900 },
+      "R3": { x: 1600, y: 1000, width: 1300, height: 1300 },
     },
     "Back": {
       "R1": { x: 1500, y: 1000, width: 500, height: 500 },
       "R2": { x: 1500, y: 1200, width: 900, height: 900 },
-      "R3": { x: 1200, y: 1000, width: 1650, height: 1650 },
+      "R3": { x: 800, y: 500, width: 1650, height: 1650 },
     },
   },
   "Remera Clásica": {
